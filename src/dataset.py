57e2e773b8a852b0211f09445567956fb7b6c625
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-01-26 21:16:45 -0500

    add notebook shell

---
+++ src/dataset.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "Name": ["Tolu", "Sefunmi", "Eden"],
+    "Age": [32, 32, 2],
+    "City": [
+        "Laurel",
+        "Baltimore",
+        "Howard",
+    ],
+}
+
+df = pd.DataFrame(data)
